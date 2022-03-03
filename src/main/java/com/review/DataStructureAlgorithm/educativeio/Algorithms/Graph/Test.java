8f9bf45980505d9d6fe55c6a9f501b8c38cc3a67
Ela <elavarasi.dc@gmail.com>
2022-03-02 21:56:47 -0800

    test.js

--- src/main/java/com/review/DataStructureAlgorithm/educativeio/Algorithms/Graph/Test.java
+++ src/main/java/com/review/DataStructureAlgorithm/educativeio/Algorithms/Graph/Test.java
@@ -1,11 +1,35 @@
 package com.review.DataStructureAlgorithm.educativeio.Algorithms.Graph;
 
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Test {
 
     public static void main(String args[]) {
+
+//        PriorityQueue<Product> queue = new PriorityQueue<Product>((a, b) -> b.noOfItemsSold - a.noOfItemsSold == 0 ? a.price - b.price :  b.noOfItemsSold - a.noOfItemsSold);
+//        queue.add(new Product("ela", 2, 4));
+//        queue.add(new Product("harshitha", 3, 4));
+//        queue.add(new Product("tharah", 2, 6));
+//        queue.add(new Product("kanag", 4, 7));
+
+        LinkedList<Product> list1 = new LinkedList<Product>();
+        list1.add(new Product("ela", 2, 7));
+        list1.add(new Product("harshitha", 3, 4));
+        list1.add(new Product("tharah", 2, 6));
+        list1.add(new Product("kanag", 4, 7));
+
+        Collections.sort(list1, (o1, o2) -> {
+            if (o2.noOfItemsSold - o1.noOfItemsSold == 0) {
+                return o1.price - o2.price;
+            }
+            return o2.noOfItemsSold - o1.noOfItemsSold;
+        });
+
+        Iterator<Product> itr1 = list1.iterator();
+        while (itr1.hasNext()) {
+            System.out.println(itr1.next().name);
+        }
+
         LinkedList<Integer> list = new LinkedList<Integer>();
         list.add(1);
         list.add(2);
@@ -33,6 +57,18 @@ public class Test {
             System.out.println(eachItem);
         }
 
+    }
+
+    static class Product {
+        String name;
+        int noOfItemsSold;
+        int price;
+
+        Product(String s, int n, int p) {
+            name = s;
+            noOfItemsSold = n;
+            price = p;
+        }
     }
 
 }
